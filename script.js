23c6cebaefd5b0fbd9369a71bcfdbc5a82eae2ef
Kamakhya Soni <kamakhyasoni085@gmail.com>
2026-02-13 18:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function insert_Row() {
 
-    // Get table
-    let table = document.getElementById("sampleTable");
 
-    // Insert row at TOP
-    let newRow = table.insertRow(0);
-
-    // Insert cells
-    let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-
-    // Add text
-    cell1.innerHTML = "New Cell1";
-    cell2.innerHTML = "New Cell2";
-}
 
 
